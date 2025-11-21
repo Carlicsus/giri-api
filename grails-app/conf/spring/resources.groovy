4e401a0ef8e34c2d23a35a0d86d41a748b17043d
Carlicsus <carlicsus3@gmail.com>
2025-11-21 13:11:46 -0600

    Login funcional

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,5 @@
+import com.giri.security.AppUserPasswordEncoderListener
 // Place your Spring DSL code here
 beans = {
+    appUserPasswordEncoderListener(AppUserPasswordEncoderListener)
 }
--- grails-app/init/giri/api/BootStrap.groovy
+++ grails-app/init/giri/api/BootStrap.groovy
@@ -1,8 +1,28 @@
 package giri.api
+import com.giri.security.AppUser
+import com.giri.security.AppUserRole
+import com.giri.security.Role
 
 class BootStrap {
 
     def init = { servletContext ->
+
+        def adminRole = Role.findOrSaveByAuthority('ROLE_ADMIN')
+        def userRole = Role.findOrSaveByAuthority('ROLE_USER')
+
+        def testUser = AppUser.findOrSaveByUsernameAndPassword('me', 'password')
+
+        AppUserRole.create testUser, adminRole
+
+        AppUserRole.withSession {
+            it.flush()
+            it.clear()
+        }
+
+        assert AppUser.count() == 1
+        assert Role.count() == 2
+        assert AppUserRole.count() == 1
+
     }
     def destroy = {
     }
